99c6970b0de3a761931421dde9eca1fedc957833
James Webster <jim@james-webster.com>
2010-06-19 14:29:29 +1000

    Updated code and project to reflect that it is licensed under the Mozilla Public License 1.1

--- AgileWallaby.Ehcache.Test/Properties/AssemblyInfo.cs
+++ AgileWallaby.Ehcache.Test/Properties/AssemblyInfo.cs
@@ -1,4 +1,30 @@
-﻿using System.Reflection;
+﻿#region License
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Ehcache .Net Wrapper..
+ *
+ * The Initial Developer of the Original Code is
+ * James Webster.
+ * Portions created by the Initial Developer are Copyright (C) 2010
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *
+ * ***** END LICENSE BLOCK ***** */
+#endregion License
+
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -9,7 +35,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("AgileWallaby.Ehcache")]
-[assembly: AssemblyCopyright("Copyright © James Webster 2010")]
+[assembly: AssemblyCopyright("Copyright © James Webster 2010. Licensed under the Mozilla Public License 1.1")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
