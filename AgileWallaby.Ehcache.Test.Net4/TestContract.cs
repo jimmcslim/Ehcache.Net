0e6cd37c709dbe791a85754d00ddb49e08c12597
James Webster <jim@james-webster.com>
2010-06-26 17:11:11 +1000

    A number of minor changes.

---
+++ AgileWallaby.Ehcache.Test.Net4/TestContract.cs
@@ -0,0 +1,40 @@
+#region License
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Ehcache .Net Wrapper..
+ *
+ * The Initial Developer of the Original Code is
+ * James Webster.
+ * Portions created by the Initial Developer are Copyright (C) 2010
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *
+ * ***** END LICENSE BLOCK ***** */
+#endregion License
+
+using System.Runtime.Serialization;
+
+namespace AgileWallaby.Ehcache.Test
+{
+    [DataContract]
+    public class TestContract
+    {
+        [DataMember]
+        public string PropertyOne { get; set; }
+
+        [DataMember]
+        public int PropertyTwo { get; set; }        
+    }
+}
